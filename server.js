6254e2567fe6cf3bc1f0c42c2188d2a55c6e185b
Rajagopal37 <trajagopal37@gmail.com>
2024-09-25 14:02:15 +0530

    method add

--- server.js
+++ server.js
@@ -12,8 +12,12 @@ const PORT = process.env.PORT || 5000;
 
 //Configure CORS to allow requests from your frontend
 app.use(cors({
-    origin: 'https://tkapfrdus.netlify.app/',  // Replace with frontend URL
+    origin: 'https://tkapfrdus.netlify.app/', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true,  
   }));
+
+app.options('*', cors());
 
 // Middleware to parse JSON
 app.use(express.json());
