3790cb045362cee3e85fcb5e101e4dde4a0975f2
Rajagopal37 <trajagopal37@gmail.com>
2024-09-25 10:41:31 +0530

    fontend add

--- server.js
+++ server.js
@@ -4,19 +4,16 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes'); // Import task routes
 const auth = require('./middleware/authMiddleware');
+const cors = require('cors'); // Import cors here
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-//-----------------
-
 //Configure CORS to allow requests from your frontend
-// app.use(cors({
-//     origin: 'https://tkapfrdus.netlify.app/',  // Replace with frontend URL
-//   }));
-
-//-----------------------
+app.use(cors({
+    origin: 'https://tkapfrdus.netlify.app/',  // Replace with frontend URL
+  }));
 
 // Middleware to parse JSON
 app.use(express.json());
